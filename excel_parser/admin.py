4407477c898336a4e461fffbdfaed1ec2a4e62fc
Shols232 <62092484+shols232@users.noreply.github.com>
2020-06-20 14:57:45 +0100

    Testing (#10)
    * 'refactored code for easy db storage'
    
    * 'refactored code for easy DB storage'
    
    * 'refactored code for easy DB storage'
    
    Co-authored-by: shols232 <akinsolaademolatemitope@gmail.com>

--- excel_parser/admin.py
+++ excel_parser/admin.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 from django.contrib import admin
+from .models import Budget
 
-# Register your models here.
-=======
-from django.contrib import admin
-
-# Register your models here.
->>>>>>> development
+admin.site.register(Budget)
--- excel_parser/views.py
+++ excel_parser/views.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 from rest_framework import viewsets 
 from .models import Budget
-from .serializers import BudgetSerializer
-
-class BudgetView(viewsets.ModelViewSet): 
-	queryset = Budget.objects.all() # this code is to call all object from the db 
-	serializer_class = BudgetSerializer # this code use the class defined in the serializers.py
-
-# Create your views here.
-=======
 import pandas
 import json
 import numpy as np
@@ -18,17 +9,21 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 from rest_framework import status
-from django.utils.datastructures import MultiValueDictKeyError
 from django.conf import settings
-from .models import ExcelSaverModel
+from .models import ExcelSaverModel, Budget
+from datetime import datetime
 import os
+from .serializers import BudgetSerializer
+
+class BudgetView(viewsets.ModelViewSet): 
+	queryset = Budget.objects.all() # this code is to call all object from the db 
+	serializer_class = BudgetSerializer # this code use the class defined in the serializers.py
 
 # get project media url
 media_url = settings.MEDIA_URL
 
-# this code block assumes only one file is sent but can easily be refactored to work on several files
+# this code block accepts several files at once or just one
 # this only works for daily payment reports excel sheets
-# accepts a GET request with parameter daily-report = True, year, month and day to get all daily files for that day
 
 @api_view(['POST', 'GET'])
 def daily_payment_report_view(request):
@@ -37,136 +32,63 @@ def daily_payment_report_view(request):
 
 
     if request.method == 'POST':
-        try:
-            excel_files = request.FILES.getlist("excel_file")
-            for current_excel_file in excel_files:
-                excel_file_name = current_excel_file.name
-                date = excel_file_name.split('-')
-                day = date[0]
-                month = date[1]
-                year = date[2].split('.')[0]
-                current_file_path = f'media/daily/{year}/{month}/{day}/{excel_file_name}'
+        excel_files = request.FILES.getlist("excel_file")
+        for current_excel_file in excel_files:
+            excel_file_name = current_excel_file.name
+            date = excel_file_name.split('-')
+            day = date[0]
+            month = date[1]
+            year = date[2].split('.')[0]
+            if len(year) < 3:
+                year = '20' + year
+            current_file_path = f'media/daily/{excel_file_name}'
+            print(current_file_path)
+            if os.path.exists(current_file_path):
+                #  code to make sure all files are unique
+                continue
+            elif excel_file_name[-3:] == 'xls' or excel_file_name[-4:] == 'xlsx':
+                ExcelSaverModel.objects.get_or_create(daily_report_file=current_excel_file)
+                daily_files_url = media_url + f'daily/'
 
-                if os.path.exists(current_file_path):
-                    #  code to make sure all files are unique
-                    continue
-                elif excel_file_name[-3:] == 'xls' or excel_file_name[-4:] == 'xlsx':
-                    ExcelSaverModel.objects.create(daily_report_file=current_excel_file)
-                    # more logic to validate stored files in db may be done here if necessary
+                # gets all files in daily folder
+                for daily_file in os.listdir(daily_files_url):
+                    try:
+                        required_file_path = daily_files_url + f'/{daily_file}'
+                        data = pandas.read_excel(required_file_path, sheet_name=0, usecols='C:F')
+                        data1 = data.dropna(axis=0, how='all', thresh=3)
+                        data2 = data1.dropna(axis=1, how='all')
+                        try:
+                            data2.columns = data2.iloc[0]
+                            if 'Description' in data2.columns:
+                                data2 = data2.iloc[1:, ].reindex()
 
-                else:
-                    #file extension not supported
-                    # return Response(status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-                    continue
-            return Response(status=status.HTTP_200_OK)
-        except MultiValueDictKeyError:
-            # in case user uploaded more than one file
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'GET' and request.GET.get('daily-reports'):
-        # variable that stores all sheet data in variable in python dict format
-        # contains the data to be stored in the database
+                            else:
+                                data2.columns = data2.iloc[1]
+                                data2 = data2.iloc[2:, ].reindex()
+                        except IndexError:
+                            continue
 
-        year = request.GET.get('year')
-        month = request.GET.get('month')
+                        data2.columns = data2.columns.str.lower()
+                        df = data2.rename(columns={'beneficiary name': 'project_recipient_name', 'amount': 'project_amount',
+                                                    'description': 'project_description', 'organization name': 'organization_name'})
 
-        daily_files_url = media_url + f'daily/'
-        required_files_dir = daily_files_url + f'{year}/{month}'
-        daily_expenses = {}
+                        name = f'{day}-{month}-{year}'
+                        date = datetime.strptime(name, '%d-%m-%Y').date()
+                        df['project_date'] = date
+                        print(data2[:6])
+                        valid_data = ['JAN', 'FEB', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+                        pattern = ' 20|'.join(valid_data)
+                        df['project_description'] = np.where(df['project_description'].str.contains(pattern, na=False),
+                                                             df['project_description'].str[10:],
+                                                             df['project_description'])
+                        df['MDA_name'] = 'FEDERAL GOVERNMENT'
 
-        # will be refactored into function soon
-        if request.GET.get('day'):
-            day = request.GET.get('day')
-            file_dir = required_files_dir + f'/{day}'
-            try:
-                current_file_name = os.listdir(file_dir)[0]
-                current_file = file_dir + f'/{current_file_name}'
-            except FileNotFoundError:
-                return Response(status=status.HTTP_404_NOT_FOUND)
-            data = pandas.read_excel(current_file, sheet_name=0, usecols='C:F')
-            data1 = data.dropna(axis=0, how='all', thresh=3)
-            data2 = data1.dropna(axis=1, how='all')
+                        # store data in dict form. this is the data to loop over to store into db
+                        daily_expenses = df.to_dict(orient='records')
+                    except KeyError:
+                        continue
 
-            try:
-                data2.columns = data2.iloc[0]
+                    # code to store into database should be done here...
 
-                if 'Description' in data2.columns:
-                    pass
-                else:
-                    data2.columns = data2.iloc[1]
-                data2 = data2.iloc[2:, ].reindex()
-            except IndexError:
-                return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_200_OK)
 
-            df = data2.rename(columns={'Beneficiary Name': 'project_recipient_name', 'Amount': 'project_amount',
-                                       'Description': 'project_description'})
-            print(df[:5])
-            df['project_date'] = df['project_description'].str[:8]
-            valid_data = ['JAN', 'FEB', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
-            pattern = ' 20|'.join(valid_data)
-            df['project_date'] = np.where(df['project_description'].str.contains(pattern, na=False),
-                                          df['project_description'].str[:8], '')
-            df['project_description'] = np.where(df['project_description'].str.contains(pattern, na=False),
-                                                 df['project_description'].str[10:],
-                                                 df['project_description'])
-            df['MDA_name'] = 'FEDERAL GOVERNMENT'
-            df['project_date'] = df['project_date'].astype(str)
-            value = str(current_file_name)
-
-            # make dict identifier filename without its extension
-            if 'xls' in current_file_name[-3:]:
-                value = str(current_file_name)[:-4]
-            if 'xlsx' in current_file_name[-4:]:
-                value = str(current_file_name)[:-5]
-            print(value)
-
-            # store data in dict form
-            daily_expenses[value] = df.to_dict(orient='records')
-        else:
-            # gets all files in daily folder
-            for daily_file in os.listdir(required_files_dir) :
-                current_file_dir = required_files_dir + f'/{daily_file}'
-                current_file_name = os.listdir(current_file_dir)[0]
-                current_file = current_file_dir + f'/{current_file_name}'
-                data = pandas.read_excel(current_file, sheet_name=0, usecols='C:F')
-                data1 = data.dropna(axis=0, how='all', thresh=3)
-                data2 = data1.dropna(axis=1, how='all')
-
-                try:
-                    data2.columns = data2.iloc[0]
-
-                    if 'Description' in data2.columns:
-                        pass
-                    else:
-                        data2.columns = data2.iloc[1]
-                    data2 = data2.iloc[2:, ].reindex()
-                except IndexError:
-                    return Response(status=status.HTTP_204_NO_CONTENT)
-
-                df = data2.rename(columns={'Beneficiary Name': 'project_recipient_name', 'Amount':'project_amount', 'Description': 'project_description'})
-                print(df[:5])
-                df['project_date'] = df['project_description'].str[:8]
-                valid_data = ['JAN', 'FEB', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
-                pattern = ' 20|'.join(valid_data)
-                df['project_date'] = np.where(df['project_description'].str.contains(pattern, na=False), df['project_description'].str[:8], '')
-                df['project_description'] = np.where(df['project_description'].str.contains(pattern, na=False), df['project_description'].str[10:],
-                                         df['project_description'])
-                df['MDA_name'] = 'FEDERAL GOVERNMENT'
-                df['project_date'] = df['project_date'].astype(str)
-                value = str(daily_file)
-
-                # make dict identifier filename without its extension
-                if 'xls' in daily_file[-3:]:
-                    value = str(daily_file)[:-4]
-                if 'xlsx' in daily_file[-4:]:
-                    value = str(daily_file)[:-5]
-                print(value)
-                # store data in dict form
-                daily_expenses[value] = df.to_dict(orient='records')
-
-        # variable containing data in json format
-        json_data = json.dumps(daily_expenses)
-
-
-        # for testing , return the data in Json format as response until db is made
-        return Response(json_data, status=status.HTTP_200_OK)
->>>>>>> development
