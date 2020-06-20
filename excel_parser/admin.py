f6c054e976b3e3971dec1d4ebcb27762b5b7108f
shols232 <akinsolaademolatemitope@gmail.com>
2020-06-20 14:39:49 +0100

    'refactored code for easy DB storage'

--- excel_parser/admin.py
+++ excel_parser/admin.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-from django.contrib import admin
-
-# Register your models here.
-=======
 from django.contrib import admin
 from .models import Budget
 
 admin.site.register(Budget)
-# Register your models here.
->>>>>>> development
--- microservice/urls.py
+++ microservice/urls.py
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-"""microservice URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-from rest_framework.documentation import include_docs_urls
-from rest_framework.schemas import get_schema_view
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-=======
 """microservice URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -40,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
 
 API_TITLE = 'Microservice API'
 
@@ -51,4 +28,4 @@ urlpatterns = [
     path('schema/', schema_view),
     path('docs/', include_docs_urls(title="MicroService")),
 ]
->>>>>>> development
+
