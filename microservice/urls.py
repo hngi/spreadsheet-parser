ef5ddde8185b5ff93f3f36066693da8e8bd450c6
Olaiya Ifedapo Princewill <princewillolaiya@gmail.com>
2020-06-20 12:44:53 +0100

    Added the documentation

--- microservice/urls.py
+++ microservice/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path 
+from django.urls import path
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -39,8 +41,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+API_TITLE = 'Microservice API'
+
+schema_view = get_schema_view(title=API_TITLE)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('excel_parser.urls')),
+    path('schema/', schema_view),
+    path('docs/', include_docs_urls(title="MicroService")),
 ]
 >>>>>>> development
