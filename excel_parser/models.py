6227da39412d94b8ffa2302a8eba3a9215b74f4a
shols232 <akinsolaademolatemitope@gmail.com>
2020-06-20 14:50:47 +0100

    'refactored code for easy DB storage'

--- excel_parser/models.py
+++ excel_parser/models.py
@@ -1,8 +1,11 @@
-<<<<<<< HEAD
 from django.db import models
 
 # Create your models here.
+def upload_file_handler(instance, filename):
+    return f'daily/{filename}'
 
+class ExcelSaverModel(models.Model):
+    daily_report_file = models.FileField(upload_to=upload_file_handler, null=True)
 
 class Budget(models.Model):
     MDA_name = models.CharField(max_length=45)
@@ -10,26 +13,6 @@ class Budget(models.Model):
     project_name = models.TextField()
     project_amount = models.FloatField()
     project_date = models.DateTimeField()
-=======
-from django.db import models
-
-def upload_file_handler(instance, filename):
-    date = filename.split('-')
-    day = date[0]
-    month = date[1]
-    year = date[2].split('.')[0]
-    if len(year) < 3:
-        year = '20'+year
-    return f'daily/{filename}'
-class ExcelSaverModel(models.Model):
-    daily_report_file = models.FileField(upload_to=upload_file_handler, null=True)
 
 
 
-class Budget(models.Model):
-    MDA_name = models.CharField(max_length=120)
-    project_recipient_name = models.CharField(max_length=120)
-    project_name = models.TextField()
-    project_amount = models.FloatField()
-    project_date = models.DateTimeField()
->>>>>>> development
