19afa958d5f171b8601ce2d7d2f93abf7d2baea4
Frank Moses <55638637+frank9th@users.noreply.github.com>
2020-06-21 22:37:58 +0100

    update path
    updated path, micro service

--- microservice/urls.py
+++ microservice/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('excel_parser.urls')),
     path('schema/', schema_view),
-    path('docs/', include_docs_urls(title="MicroService")),
+    path('', include_docs_urls(title="MicroService")),
 ]
