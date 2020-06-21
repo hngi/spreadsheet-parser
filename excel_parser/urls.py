e15b1f546177a05f8dc6895ded24d24feb68dbe6
Frank Moses <55638637+frank9th@users.noreply.github.com>
2020-06-21 22:17:22 +0100

    Update Urls.py
    registered router for daily payment and report

--- excel_parser/urls.py
+++ excel_parser/urls.py
@@ -4,7 +4,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 router = routers.DefaultRouter()
-router.register("budget", views.BudgetView)
+router.register("home", views.BudgetView)
+router.register("daily-payment-report", views.daily_payment_report_view),
+router.register("daily_reports_view", views.get_daily_reports_view), 
 
 
 urlpatterns = [
