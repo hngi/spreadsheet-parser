ec34fe770be8a990a9e859921fc595dec82f0364
MarlineKhavele <marlinekhavele6@gmail.com>
2020-06-21 22:03:20 +0300

    commented db

--- microservice/settings.py
+++ microservice/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = 'microservice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'parser',
-        'USER': "postgres",
-        'PASSWORD': "password",
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'parser',
+#         'USER': "postgres",
+#         'PASSWORD': "password",
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
