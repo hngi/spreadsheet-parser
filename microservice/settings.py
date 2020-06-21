0a6503fd1a64124a6b12d6588e1a078825b58060
MarlineKhavele <marlinekhavele6@gmail.com>
2020-06-21 18:55:19 +0300

    working with my db

--- microservice/settings.py
+++ microservice/settings.py
@@ -85,18 +85,18 @@ WSGI_APPLICATION = 'microservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': "django.db.backends.postgresql_psycopg2",
-        # 'NAME': "parser",
-        # 'USER': "postgres",
-        # 'PASSWORD':"password",
-        # 'HOST': "localhost",
-        # 'PORT': "5432",
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hvdptmsr',
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': 'ruby.db.elephantsql.com',
-        'PORT': '5432'
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'NAME': "parser",
+        'USER': "postgres",
+        'PASSWORD':"password",
+        'HOST': "localhost",
+        'PORT': "5432",
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'hvdptmsr',
+        # 'USER': os.getenv('USER'),
+        # 'PASSWORD': os.getenv('PASSWORD'),
+        # 'HOST': 'ruby.db.elephantsql.com',
+        # 'PORT': '5432'
     }
 }
 
