c592d06a86b33f50d0ea76c78a2c2aaa0dd7daad
Oluwadamilola Sonaike <56271955+damiso15@users.noreply.github.com>
2020-06-26 14:12:08 +0100

    created views to query db of all data (#48) (#49)
    Co-authored-by: Frank Moses <55638637+frank9th@users.noreply.github.com>

--- monthly_budget/views.py
+++ monthly_budget/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+# imported serializers class MonthlySerializer from serializers.py 
+from excel_parser.serializers import MonthlySerializer
+
 # Create your views here.
+class MonthlyView(viewsets.ModelViewSet):
+	queryset = AdministrativeBudget.objects.all() # this code is to call all object from the db
+	serializer_class = MonthlySerializer # this code use the class defined in the serializers.py
