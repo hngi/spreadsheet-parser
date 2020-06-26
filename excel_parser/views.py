4bfae1944ad138ab5747e71de4a022201bedadcf
shols232 <akinsolaademolatemitope@gmail.com>
2020-06-26 19:10:08 +0100

    feat: functionality to query DB for list of MDA transactions

--- excel_parser/views.py
+++ excel_parser/views.py
@@ -14,6 +14,7 @@ from django.conf import settings
 from .models import ExcelSaverModel, Budget
 from datetime import datetime
 import os
+from django.db.models import Q
 from .serializers import BudgetSerializer
 
 class BudgetView(viewsets.ModelViewSet):
@@ -129,7 +130,6 @@ def get_daily_reports_view(request):
                 qs = Budget.objects.filter(project_date=date)
             except ValueError:
                 return Response("Wrong Date Format")
-        print(qs)
         serializer = BudgetSerializer(qs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
