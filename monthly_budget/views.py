cbd2837c28d45105027e1325cba6ed263ba7debe
Oluwadamilola Sonaike <56271955+damiso15@users.noreply.github.com>
2020-06-26 23:42:11 +0100

    Government Function Table (#64)
    * pushing to my code base
    
    * pushing to my code base
    
    * wrote the code for the tables for monthly Revenue and Expenditure
    
    * created the last table

--- monthly_budget/views.py
+++ monthly_budget/views.py
@@ -1,36 +1,49 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from rest_framework import mixins
 from rest_framework import generics
 from .models import MDABudget, AdministrativeBudget
 from .serializers import MDABudgetSerializer, MonthlySerializer
 
+
 # Create your views here.
+
+
 class MonthlyView(viewsets.ModelViewSet):
-	queryset = AdministrativeBudget.objects.all() # this code is to call all object from the db
-	serializer_class = MonthlySerializer # this code use the class defined in the serializers.py
-	
+    queryset = AdministrativeBudget.objects.all()  # this code is to call all object from the db
+    serializer_class = MonthlySerializer  # this code use the class defined in the serializers.py
+
+
 '''
-this function when called, takes json data in the format [{"mda":"Test","budget":100000,"allocation":27934783353.5,"total_allocation":686919637.6900000572,"balance":2106428472.8900001049},{"mda":"Test","budget":100000,"allocation":27934783353.5,"total_allocation":686919637.6900000572,"balance":2106428472.8900001049},{"mda":"Test","budget":100000,"allocation":27934783353.5,"total_allocation":686919637.6900000572,"balance":2106428472.8900001049}] and saves each of the rows to the database.
-'''	
-def savemda(json_data):
-    lenght = len(json_data)
-    for i in range(lenght):
+this function when called, takes json data in the format 
+[{"mda":"Test","budget":100000,"allocation":27934783353.5,"total_allocation":686919637.6900000572,
+"balance":2106428472.8900001049},{"mda":"Test","budget":100000,"allocation":27934783353.5,
+"total_allocation":686919637.6900000572,"balance":2106428472.8900001049},
+{"mda":"Test","budget":100000,"allocation":27934783353.5,"total_allocation":686919637.6900000572,
+"balance":2106428472.8900001049}] and saves each of the rows to the database.
+'''
+
+
+def save_mda(json_data):
+    length = len(json_data)
+    for i in range(length):
         row = json_data[i]
         serializer = MDABudgetSerializer(data=row)
         if serializer.is_valid():
-
             serializer.save()
             return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)     
+        return JsonResponse(serializer.errors, status=400)
+
 
 '''
 added a C.B view for returning a list of all MDA transactions available in the database
 assumed a serializer of name MDABudgetSerializer has already been made.
 '''
-class MDABudgetView(mixins.ListModelMixin,generics.GenericAPIView):
+
+
+class MDABudgetView(mixins.ListModelMixin, generics.GenericAPIView):
     queryset = MDABudget.objects.all()
     serializer_class = MDABudgetSerializer
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
-
