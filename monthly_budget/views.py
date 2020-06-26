7b151f6d29c1ce338d683431df2fac0981bd1ee2
shols232 <akinsolaademolatemitope@gmail.com>
2020-06-26 19:01:59 +0100

    feat: functionality to query DB for list of MDA transactions

--- monthly_budget/views.py
+++ monthly_budget/views.py
@@ -1,9 +1,20 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 from rest_framework import mixins
 from rest_framework import generics
 from .models import MDABudget
+# imported serializers class MonthlySerializer from serializers.py
+from excel_parser.serializers import MonthlySerializer
 
+# Create your views here.
+class MonthlyView(viewsets.ModelViewSet):
+	queryset = AdministrativeBudget.objects.all() # this code is to call all object from the db
+	serializer_class = MonthlySerializer # this code use the class defined in the serializers.py
+
+
+'''
+added a C.B view for returning a list of all MDA transactions available in the database
+assumed a serializer of name MDABudgetSerializer has already been made.
+'''
 class MDABudgetView(mixins.ListModelMixin,generics.GenericAPIView):
     queryset = MDABudget.objects.all()
     serializer_class = MDABudgetSerializer
@@ -11,14 +22,3 @@ class MDABudgetView(mixins.ListModelMixin,generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
-=======
-from django.shortcuts import render
-
-# imported serializers class MonthlySerializer from serializers.py 
-from excel_parser.serializers import MonthlySerializer
-
-# Create your views here.
-class MonthlyView(viewsets.ModelViewSet):
-	queryset = AdministrativeBudget.objects.all() # this code is to call all object from the db
-	serializer_class = MonthlySerializer # this code use the class defined in the serializers.py
->>>>>>> f670f8e84fd42592f7fe709c228d8b7fd330ab42
