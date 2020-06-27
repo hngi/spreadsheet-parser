3212045f9b237f89c79ef3eba1d94fb3dfb0ef1c
Oluwadamilola Sonaike <56271955+damiso15@users.noreply.github.com>
2020-06-27 21:34:58 +0100

    Resolve conflict (#87)
    * pushing to my code base
    
    * pushing to my code base
    
    * wrote the code for the tables for monthly Revenue and Expenditure
    
    * created the last table
    
    * changed and corrected some errors
    
    * changed and corrected some errors
    
    * created the last table
    
    * commiting conflict
    
    * changed and corrected some errors
    
    * commiting conflict
    
    * changed and corrected some errors
    
    * created the last table
    
    * created the last table
    
    * changed and corrected some errors
    
    * changed and corrected some errors

--- monthly_budget/models.py
+++ monthly_budget/models.py
@@ -69,7 +69,7 @@ class MDABudget(models.Model):
     allocation = models.FloatField(max_length=50, null=True)
     total_allocation = models.FloatField(max_length=50, null=True)
     balance = models.FloatField(max_length=50, null=True)
-    month =  models.CharField(max_length=20, null=True)
+    month = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.mda
