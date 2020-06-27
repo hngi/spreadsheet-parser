c03210dc735d1ebd216cf2cec1e96fc14cedd51b
damiso15 <damiso15@yahoo.com>
2020-06-27 10:27:41 +0100

    changed and corrected some errors

--- monthly_budget/views.py
+++ monthly_budget/views.py
@@ -14,6 +14,7 @@ from .models import MDABudget, AdministrativeBudget
 from .serializers import MDABudgetSerializer, MonthlySerializer
 # imported serializers class MonthlySerializer from serializers.py 
 from excel_parser.serializers import MonthlySerializer
+
 media_url = settings.MEDIA_URL
 
 
@@ -21,7 +22,6 @@ media_url = settings.MEDIA_URL
 
 
 class MonthlyView(viewsets.ModelViewSet):
-<<<<<<< HEAD
     queryset = AdministrativeBudget.objects.all()  # this code is to call all object from the db
     serializer_class = MonthlySerializer  # this code use the class defined in the serializers.py
 
@@ -45,10 +45,9 @@ def save_mda(json_data):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
-=======
-	queryset = AdministrativeBudget.objects.all() # this code is to call all object from the db
-	serializer_class = MonthlySerializer # this code use the class defined in the serializers.py
->>>>>>> ccf35db6ad79d2d62f6b62a7c6ffbdfddae8a914
+
+        queryset = AdministrativeBudget.objects.all()  # this code is to call all object from the db
+        serializer_class = MonthlySerializer  # this code use the class defined in the serializers.py
 
 
 '''
@@ -63,63 +62,54 @@ class MDABudgetView(mixins.ListModelMixin, generics.GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
-<<<<<<< HEAD
-=======
 
 
+def administrative_budget(request):
+    """
+    A Views Function that extracts data from the database and store as a list of dictionaries, to make it easy to be
+    stored into the database. If you are to assigned to store in database please be aware that the file is stored in
+    'final_data' and the month is stored in 'month' . cheers from ferrum
+    """
+    excel_files = request.FILES.getlist("excel_file")
 
-
-
-def monthly_report(request):
-	"""
-	A Views Function that extracts data from the database and store as a list of dictionaries, to make it easy to be stored into the database
-	If you are to assigned to store in database
-	please be aware that the file is stored in 'final_data' and the month is stored in 'month' . cheers from ferrum
-	"""
-	excel_files = request.FILES.getlist("excel_file")
-
-	# a loop to get the files from the media folder
-	for current_excel_file in excel_files:
-		excel_file_name = current_excel_file.name
-		current_file_path = f'media/monthly/{excel_file_name}'
-		if os.path.exists(current_file_path):
-			continue
-		elif excel_file_name[-3:] == 'xls' or excel_file_name[-4:] == 'xlsx':
-			ExcelSaverModel.objects.get_or_create(monthly_file=current_excel_file)
-			monthly_files_url = media_url + f'monthly/'
+    # a loop to get the files from the media folder
+    for current_excel_file in excel_files:
+        excel_file_name = current_excel_file.name
+        current_file_path = f'media/monthly/{excel_file_name}'
+        if os.path.exists(current_file_path):
+            continue
+        elif excel_file_name[-3:] == 'xls' or excel_file_name[-4:] == 'xlsx':
+            ExcelSaverModel.objects.get_or_create(monthly_file=current_excel_file)
+            monthly_files_url = media_url + f'monthly/'
 
             # gets all files in Monthly folder
-			for file in os.listdir(monthly_files_url):
-				try:
-					absolute_file_path = monthly_files_url +f"/{file}"
-					# reading the excel file
-					df = pd.read_excel(absolute_file_path, usecols = "B:G",encoding='utf-8' )
-					# Dropping the unneccessary columns
-					data = df.dropna(axis = 0, how= "any")
-					data.columns = data.iloc[0]
-					data2 = data.iloc[1:,].reindex()
-					# here is month, the variable in which the month is stored in
-					month = data2.columns[2]
-					data2.columns = data2.columns.map(lambda x: x.replace('\n', ''))
-					data2.columns = ["sector", "budget", "allocation","total_allocation","balance","percentage"]
-					# we dont need percentage... dropping it
-					data2.drop(["percentage"], axis = 1, inplace = True)
+            for file in os.listdir(monthly_files_url):
+                try:
+                    absolute_file_path = monthly_files_url + f"/{file}"
+                    # reading the excel file
+                    df = pd.read_excel(absolute_file_path, usecols="B:G", encoding='utf-8')
+                    # Dropping the unneccessary columns
+                    data = df.dropna(axis=0, how="any")
+                    data.columns = data.iloc[0]
+                    data2 = data.iloc[1:, ].reindex()
+                    # here is month, the variable in which the month is stored in
+                    month = data2.columns[2]
+                    data2.columns = data2.columns.map(lambda x: x.replace('\n', ''))
+                    data2.columns = ["sector", "budget", "allocation", "total_allocation", "balance", "percentage"]
+                    # we dont need percentage... dropping it
+                    data2.drop(["percentage"], axis=1, inplace=True)
 
-					# formatting the floats to make sure they all have uniform decimal points
-					#initially they are returning floats in the form of exponentials.
-					data2["budget"] = data2["budget"].apply(lambda x: "{:.2f}".format(x))
-					data2["allocation"] = data2["allocation"].apply(lambda x: "{:.2f}".format(x))
-					data2["total_allocation"] = data2["total_allocation"].apply(lambda x: "{:.2f}".format(x))
-					data2["balance"] = data2["balance"].apply(lambda x: "{:.2f}".format(x))
-					# here is final_data, the list of dictionaries that can be easily stored in the database
-					final_data = data2.to_dict(orient = "records")
-				except KeyError:
-					continue
-	return Response(status=status.HTTP_200_OK)
-
-	
-
-
+                    # formatting the floats to make sure they all have uniform decimal points
+                    # initially they are returning floats in the form of exponentials.
+                    data2["budget"] = data2["budget"].apply(lambda x: "{:.2f}".format(x))
+                    data2["allocation"] = data2["allocation"].apply(lambda x: "{:.2f}".format(x))
+                    data2["total_allocation"] = data2["total_allocation"].apply(lambda x: "{:.2f}".format(x))
+                    data2["balance"] = data2["balance"].apply(lambda x: "{:.2f}".format(x))
+                    # here is final_data, the list of dictionaries that can be easily stored in the database
+                    final_data = data2.to_dict(orient="records")
+                except KeyError:
+                    continue
+    return Response(status=status.HTTP_200_OK)
 
 
 '''
@@ -141,6 +131,3 @@ def save_mda(json_data):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
-
-
->>>>>>> ccf35db6ad79d2d62f6b62a7c6ffbdfddae8a914
