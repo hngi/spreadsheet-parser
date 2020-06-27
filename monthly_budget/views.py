7887fbdb5679dbebe795f76587397558b67d48a2
damiso15 <damiso15@yahoo.com>
2020-06-27 17:02:35 +0100

    commiting conflict

--- monthly_budget/views.py
+++ monthly_budget/views.py
@@ -21,28 +21,18 @@ from django.views.decorators.csrf import csrf_exempt
 
 media_url = settings.MEDIA_URL
 
-
 # Create your views here.
 
 
-<<<<<<< HEAD
 '''
 added a C.B view for returning a list of all MDA transactions available in the database
 assumed a serializer of name MDABudgetSerializer has already been made.
 '''
-=======
-<<<<<<< HEAD
+
+
 class MonthlyView(viewsets.ModelViewSet):
     queryset = AdministrativeBudget.objects.all()  # this code is to call all object from the db
     serializer_class = AdministrativeExpensesSerializer  # this code use the class defined in the serializers.py
-
-
-=======
->>>>>>> 442756fa9aba71db5ac4a82753e2e51feeaa0cea
-'''
-added a C.B view for returning a list of all MDA transactions available in the database
-
->>>>>>> 387210e0212fe7fdb24dc982b5634138b2fa935b
 
 
 class MDABudgetView(mixins.ListModelMixin, generics.GenericAPIView):
@@ -51,7 +41,6 @@ class MDABudgetView(mixins.ListModelMixin, generics.GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
-<<<<<<< HEAD
 
 
 """
@@ -61,17 +50,6 @@ stored into the database. If you are to assigned to store in database please be 
 """
 
 
-=======
-
-
-"""
-A Views Function that extracts data from the database and store as a list of dictionaries, to make it easy to be
-stored into the database. If you are to assigned to store in database please be aware that the file is stored in
-'final_data' and the month is stored in 'month' . cheers from ferrum
-"""
-
-
->>>>>>> 387210e0212fe7fdb24dc982b5634138b2fa935b
 def administrative_budget(request):
     excel_files = request.FILES.getlist("excel_file")
 
@@ -91,7 +69,7 @@ def administrative_budget(request):
                     absolute_file_path = monthly_files_url + f"/{file}"
                     # reading the excel file
                     df = pd.read_excel(absolute_file_path, usecols="B:G", encoding='utf-8')
-                    # Dropping the unneccessary columns
+                    # Dropping the unnecessary columns
                     data = df.dropna(axis=0, how="any")
                     data.columns = data.iloc[0]
                     data2 = data.iloc[1:, ].reindex()
@@ -128,76 +106,77 @@ def get_economic_expenditure(request):
         serializer = EconomicExpenditureSerializer(qs, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     return Response({
-                    'status': 'failure',
-                    'data': {'message': 'Something went wrong'}
-                })
-<<<<<<< HEAD
-=======
-                
-         
-         
-         
+        'status': 'failure',
+        'data': {'message': 'Something went wrong'}
+    })
+
+
 '''
 This function will extract the required economic expenditure data in the expenditure table to json, like this: 
- [{"name": "SALARY", "budget": 2454037551812.8213, "allocation": 217515280304.7, "total_allocation": 854641653160.53, "balance": 1599395898652.2913},
- {"name": "NON REGULAR ALLOWANCES", "budget": 1055706358677.2299, "allocation": 66004017316.47, "total_allocation": 333894644535.48, "balance": 721811714141.7499}]
-
+ [{"name": "SALARY", "budget": 2454037551812.8213, "allocation": 217515280304.7, "total_allocation": 854641653160.53, 
+ "balance": 1599395898652.2913}, {"name": "NON REGULAR ALLOWANCES", "budget": 1055706358677.2299, 
+ "allocation": 66004017316.47, "total_allocation": 333894644535.48, "balance": 721811714141.7499}]
 NB: I added json response on lines 155 and 175 for testing purposes. 
-'''         
-         
-@api_view(['POST' ])
-def getexpenditurevalues(request):
-        excel_files = request.FILES.getlist("excel_file")
+'''
 
-        # a loop to get the files from the media folder
-        for current_excel_file in excel_files:
-            excel_file_name = current_excel_file.name
-            current_file_path = f'media/monthly/{excel_file_name}'
-            if os.path.exists(current_file_path):
-                loc = current_file_path
-                requiredvalues = []
-                wb = xlrd.open_workbook(loc)
-                sheet = wb.sheet_by_index(0)
-                numrows = sheet.nrows
-                numcols = sheet.ncols
-                for i in range(numrows):
-                    firstrowvalue = sheet.cell(i,0).value
-                    if type(firstrowvalue) == str:
-                        typeofdata = "string"
-                        # print(str(firstrowvalue) + ' is of type ' + typeofdata)
-                        if firstrowvalue.replace('.','',1).isdigit():
-                            newfirstrowvalue = int(firstrowvalue)
-                            if newfirstrowvalue > 20000000:
-                                rowcheckvalue = newfirstrowvalue
-                                rowdata = {'name': sheet.cell(i,1).value, 'budget' : sheet.cell(i,2).value, 'allocation' : sheet.cell(i,3).value, 'total_allocation' : sheet.cell(i,4).value, 'balance' : sheet.cell(i,5).value}
-                                # print(rowdata)
-                                requiredvalues.append(rowdata)
-                # print(requiredvalues)
-                return JsonResponse(requiredvalues, status=201, safe=False) 
-            elif excel_file_name[-3:] == 'xls' or excel_file_name[-4:] == 'xlsx':
-                ExcelSaverModelMonthly.objects.get_or_create(monthly_file=current_excel_file)
+
+@api_view(['POST'])
+def get_expenditure_values(request):
+    excel_files = request.FILES.getlist("excel_file")
+
+    # a loop to get the files from the media folder
+    for current_excel_file in excel_files:
+        excel_file_name = current_excel_file.name
+        current_file_path = f'media/monthly/{excel_file_name}'
+        if os.path.exists(current_file_path):
+            loc = current_file_path
+            required_values = []
+            wb = xlrd.open_workbook(loc)
+            sheet = wb.sheet_by_index(0)
+            num_rows = sheet.nrows
+            num_cols = sheet.ncols
+            for i in range(num_rows):
+                first_row_value = sheet.cell(i, 0).value
+                if type(first_row_value) == str:
+                    type_of_data = "string"
+                    # print(str(first_row_value) + ' is of type ' + type_of_data)
+                    if first_row_value.replace('.', '', 1).isdigit():
+                        new_first_row_value = int(first_row_value)
+                        if new_first_row_value > 20000000:
+                            row_check_value = new_first_row_value
+                            row_data = {'name': sheet.cell(i, 1).value, 'budget': sheet.cell(i, 2).value,
+                                        'allocation': sheet.cell(i, 3).value,
+                                        'total_allocation': sheet.cell(i, 4).value,
+                                        'balance': sheet.cell(i, 5).value}
+                            # print(row_data)
+                            required_values.append(row_data)
+            # print(required_values)
+            return JsonResponse(required_values, status=201, safe=False)
+        elif excel_file_name[-3:] == 'xls' or excel_file_name[-4:] == 'xlsx':
+            ExcelSaverModelMonthly.objects.get_or_create(monthly_file=current_excel_file)
 
             # if request.method == 'POST':
-                loc = current_file_path
-                requiredvalues = []
-                wb = xlrd.open_workbook(loc)
-                sheet = wb.sheet_by_index(0)
-                numrows = sheet.nrows
-                numcols = sheet.ncols
-                for i in range(numrows):
-                    firstrowvalue = sheet.cell(i,0).value
-                    if type(firstrowvalue) == str:
-                        typeofdata = "string"
-                        # print(str(firstrowvalue) + ' is of type ' + typeofdata)
-                        if firstrowvalue.replace('.','',1).isdigit():
-                            newfirstrowvalue = int(firstrowvalue)
-                            if newfirstrowvalue > 20000000:
-                                rowcheckvalue = newfirstrowvalue
-                                rowdata = {'name': sheet.cell(i,1).value, 'budget' : sheet.cell(i,2).value, 'allocation' : sheet.cell(i,3).value, 'total_allocation' : sheet.cell(i,4).value, 'balance' : sheet.cell(i,5).value}
-                                # print(rowdata)
-                                requiredvalues.append(rowdata)
-                # print(requiredvalues)
-                return JsonResponse(requiredvalues, status=201, safe=False) 
-            else:
-                break
->>>>>>> 387210e0212fe7fdb24dc982b5634138b2fa935b
+            loc = current_file_path
+            required_values = []
+            wb = xlrd.open_workbook(loc)
+            sheet = wb.sheet_by_index(0)
+            num_rows = sheet.nrows
+            num_cols = sheet.ncols
+            for i in range(num_rows):
+                first_row_value = sheet.cell(i, 0).value
+                if type(first_row_value) == str:
+                    type_of_data = "string"
+                    # print(str(first_row_value) + ' is of type ' + type_of_data)
+                    if first_row_value.replace('.', '', 1).isdigit():
+                        new_first_row_value = int(first_row_value)
+                        if new_first_row_value > 20000000:
+                            row_check_value = new_first_row_value
+                            row_data = {'name': sheet.cell(i, 1).value, 'budget': sheet.cell(i, 2).value,
+                                        'allocation': sheet.cell(i, 3).value,
+                                        'total_allocation': sheet.cell(i, 4).value, 'balance': sheet.cell(i, 5).value}
+                            # print(row_data)
+                            required_values.append(row_data)
+            # print(required_values)
+            return JsonResponse(required_values, status=201, safe=False)
+        else:
+            break
