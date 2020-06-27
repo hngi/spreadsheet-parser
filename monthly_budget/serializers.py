28fc31af26983bc8efe42fdf5a3d449b3704b1e5
Abdulmalik Giwa <giwa.omotola@gmail.com>
2020-06-27 13:40:23 +0100

    Serialize Economic Expenditure

---
+++ monthly_budget/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import EconomicExpenditure
+
+
+class EconomicExpenditureSerializer(serializers.ModelSerializer):
+    class Meta:
+    	model = EconomicExpenditure
+    	fields = "__all__"
