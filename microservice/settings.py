97e112a3179b4347c32d7d953dc76c540eddcc6f
shols232 <akinsolaademolatemitope@gmail.com>
2020-06-28 22:59:05 +0100

    feat:extract data from government functions excel sheets

--- microservice/settings.py
+++ microservice/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'microservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': 'ec2-35-173-94-156.compute-1.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
