6ad00a9a530c1fa2a6ab2f84609003624f3f0685
MarlineKhavele <marlinekhavele6@gmail.com>
2020-06-28 17:22:46 +0300

    Added Monthly Revenue serializer

--- monthly_budget/serializers.py
+++ monthly_budget/serializers.py
@@ -2,22 +2,53 @@
 # and serve them unto our web pages
 
 from rest_framework import serializers
-from .models import AdministrativeBudget, MDABudget,  EconomicExpenditure
+from .models import (
+    AdministrativeBudget,
+    MDABudget,
+    EconomicExpenditure,
+    EconomicRevenue,
+)
 
 
 class AdministrativeExpensesSerializer(serializers.ModelSerializer):
     class Meta:
         model = AdministrativeBudget
-        fields = ['id', 'sector', 'budget', 'allocation', 'total_allocation', 'balance']
+        fields = [
+            "id",
+            "sector",
+            "budget",
+            "allocation",
+            "total_allocation",
+            "balance",
+        ]
 
 
 class MDABudgetSerializer(serializers.ModelSerializer):
     class Meta:
         model = MDABudget
-        fields = ['id', 'mda', 'budget', 'allocation', 'total_allocation', 'balance']
+        fields = [
+            "id",
+            "mda",
+            "budget",
+            "allocation",
+            "total_allocation",
+            "balance",
+        ]
 
 
 class EconomicExpenditureSerializer(serializers.ModelSerializer):
     class Meta:
         model = EconomicExpenditure
         fields = "__all__"
+
+
+class EconomicRevenueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EconomicRevenue
+        fields = (
+            "id",
+            "name",
+            "revenue",
+            "total_renue",
+            "month",
+        )
