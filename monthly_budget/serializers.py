f1d49bb9fc34a3e2225f87ac904eb4f49a81ec32
Oluwadamilola Sonaike <56271955+damiso15@users.noreply.github.com>
2020-06-29 17:23:27 +0100

    resolved conflict (#121)
    * pushing to my code base
    
    * pushing to my code base
    
    * wrote the code for the tables for monthly Revenue and Expenditure
    
    * created the last table
    
    * changed and corrected some errors
    
    * changed and corrected some errors
    
    * created the last table
    
    * commiting conflict
    
    * changed and corrected some errors
    
    * commiting conflict
    
    * changed and corrected some errors
    
    * created the last table
    
    * created the last table
    
    * changed and corrected some errors
    
    * changed and corrected some errors
    
    * changed and corrected some errors
    
    * changed and corrected some errors
    
    * stashing stages
    
    * resolved conflicts
    
    * stashing stages
    
    * resolved conflicts
    
    * resolved conflicts
    
    * resolved conflicts
    
    * stashing stages
    
    * resolved conflicts
    
    * resolved conflict
    
    * resolving conflict
    
    * resolved conflict

--- monthly_budget/serializers.py
+++ monthly_budget/serializers.py
@@ -2,17 +2,90 @@
 # and serve them unto our web pages
 from rest_framework import serializers
 from .models import AdministrativeBudget, MDABudget, EconomicExpenditure, EconomicRevenue, GovernmentFunctions
+<<<<<<< HEAD
+from .models import (
+    AdministrativeBudget,
+    MDABudget,
+    EconomicExpenditure,
+    EconomicRevenue,
+)
+=======
+>>>>>>> cf638bdd8bdb9d46a8750faa2082e791e4d8ae6b
 
 
 class AdministrativeExpensesSerializer(serializers.ModelSerializer):
     class Meta:
         model = AdministrativeBudget
+<<<<<<< HEAD
+
+        fields = [
+            "id",
+            "sector",
+            "budget",
+            "allocation",
+            "total_allocation",
+            "balance",
+            "month",
+        ]
+=======
         fields = ['id', 'sector', 'budget', 'allocation', 'total_allocation', 'balance', 'month']
+>>>>>>> cf638bdd8bdb9d46a8750faa2082e791e4d8ae6b
 
 
 class MDABudgetSerializer(serializers.ModelSerializer):
     class Meta:
         model = MDABudget
+<<<<<<< HEAD
+        fields = [
+            "id",
+            "mda",
+            "budget",
+            "allocation",
+            "total_allocation",
+            "balance",
+            "month",
+        ]
+
+
+class EconomicRevenueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EconomicRevenue
+        fields = [
+            "id",
+            "name",
+            "revenue",
+            "total_revenue",
+            "month",
+        ]
+
+
+class EconomicExpenditureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EconomicExpenditure
+        fields = [
+            "id",
+            "name",
+            "budget",
+            "allocation",
+            "total_allocation",
+            "balance",
+            "month",
+        ]
+
+
+class GovernmentFunctionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GovernmentFunctions
+        fields = [
+            'id',
+            'name',
+            'budget',
+            'expenses',
+            'total_expenses',
+            'balance',
+            'month',
+        ]
+=======
         fields = ['id', 'mda', 'budget', 'allocation', 'total_allocation', 'balance', 'month']
 
 class EconomicRevenueSerializer(serializers.ModelSerializer):
@@ -43,3 +116,4 @@ class EconomicRevenueSerializer(serializers.ModelSerializer):
         model = EconomicRevenue
         fields = "__all__"
 
+>>>>>>> cf638bdd8bdb9d46a8750faa2082e791e4d8ae6b
