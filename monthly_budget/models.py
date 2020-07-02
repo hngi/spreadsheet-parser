1e6c77903c35976181743cc8a5387451f60b9b4d
Nwokoye Chigozie <gregoflash05@users.noreply.github.com>
2020-07-02 16:02:34 +0100

    Please effect this change on the models.py

--- monthly_budget/models.py
+++ monthly_budget/models.py
@@ -112,7 +112,7 @@ class EconomicExpenditure(models.Model):
     allocation = models.FloatField(max_length=50, null=True)
     total_allocation = models.FloatField(max_length=50, null=True)
     balance = models.FloatField(max_length=50, null=True)
-    month = real_time()
+    month = models.CharField(max_length=15, null=True)
 
     def __str__(self):
         return self.name
