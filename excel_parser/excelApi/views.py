d2fbb361b8b4a18c9a0fa177d4cc437a23e07f6c
Frank Moses <55638637+frank9th@users.noreply.github.com>
2020-07-07 19:52:16 +0100

    fixed minor bug and added Pandas version 1.0.0 to requirement (#138)

--- excel_parser/excelApi/views.py
+++ excel_parser/excelApi/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-import pandas
+import pandas as pd 
 import xlrd
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -8,5 +8,4 @@ from rest_framework.response import Response
 import os
 from rest_framework.views import APIView
 from django.views.decorators.csrf import csrf_exempt
-
 # Create your views here.
