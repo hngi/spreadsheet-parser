629df80cafc47c82598accb2d314409392601166
Frank Moses <55638637+frank9th@users.noreply.github.com>
2020-07-08 22:46:59 +0100

    Templates and static directories  (#146)
    * Added templates, update views and URLS
    
    * Added templates, update views and URLS
    
    * Created landing page for api
    
    * Created landing page for api

--- excelApi/urls.py
+++ excelApi/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 
 urlpatterns = [
+<<<<<<< HEAD
+path('', views.index, name='index'),
+path('budget.html', views.budget, name='budget'),
+  
+]
+=======
     path('', views.budget, name='budget')
 ]
+>>>>>>> c35c04aa26cc8d6570dd2a9e8936dcd98f419868
