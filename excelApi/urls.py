452e5c851b457d381752fdbd1a1d2f542b7236b4
damiso15 <damiso15@yahoo.com>
2020-07-08 22:47:19 +0100

    comit

--- excelApi/urls.py
+++ excelApi/urls.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 from django.urls import path, include
-=======
-from django.urls import path
->>>>>>> c35c04aa26cc8d6570dd2a9e8936dcd98f419868
 from . import views
-
 
 urlpatterns = [
     path('', views.budget, name='budget')
