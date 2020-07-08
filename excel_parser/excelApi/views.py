233652efc5925d6d4ddad5d7d0da85b8b2cec7f1
payn33 <amugenp2@gmail.com>
2020-07-08 01:06:01 +0100

    updates

--- excel_parser/excelApi/views.py
+++ excel_parser/excelApi/views.py
@@ -3,9 +3,13 @@ from django.http import HttpResponse, JsonResponse
 import pandas as pd 
 import xlrd
 from rest_framework import status
-from rest_framework.decorators import api_view
+#from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import os
-from rest_framework.views import APIView
+#from rest_framework.views import APIView
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+df = pd.read_excel('./media/APRIL.xlsx')
+
+return(df.head())
