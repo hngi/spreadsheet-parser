01df230d3ae6a34b32deeca340c3c3c0e300edc7
damiso15 <damiso15@yahoo.com>
2020-07-08 22:57:59 +0100

    comit

--- excelApi/urls.py
+++ excelApi/urls.py
@@ -1,13 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-path('', views.index, name='index'),
-path('budget.html', views.budget, name='budget'),
-  
+    path('', views.index, name='index'),
+    path('budget.html', views.budget, name='budget'),
 ]
-=======
-    path('', views.budget, name='budget')
-]
->>>>>>> c35c04aa26cc8d6570dd2a9e8936dcd98f419868
--- excelApi/views.py
+++ excelApi/views.py
@@ -15,57 +15,21 @@ def budget(request):
         data2 = data.iloc[1:, ].reindex()
         # data3 = df.book.nrows
         nrows = 10
-     
+
         # here is month, the variable in which the month is stored in
         # month = data2.columns[2]
         data2.columns = data2.columns.map(lambda x: x.replace('\n', ''))
         data2.columns = ["sector", "budget", "allocation", "total_allocation", "balance", "percentage"]
 
-<<<<<<< HEAD
         # we don't need percentage, dropping it
-=======
-<<<<<<< HEAD
-
-def budget(request):
-
-    try:
-       
-        # reading the excel file
-        df = pd.read_excel('./media/APRIL.xlsx', usecols = "B:G",encoding='utf-8' )
-        # Dropping the unneccessary columns
-        data = df.dropna(axis = 0, how= "any")
-        data.columns = data.iloc[0]
-        data2 = data.iloc[1:,].reindex()    
-        # data3 = df.book.nrows
-        nrows = 10
-     
-        # here is month, the variable in which the month is stored in
-        #month = data2.columns[2]
-     
-        data2.columns = data2.columns.map(lambda x: x.replace('\n', ''))
-        data2.columns = ["sector", "budget", "allocation","total_allocation","balance","percentage"]
-        # we dont need percentage... dropping it
-        data2.drop(["percentage"], axis = 1, inplace = True)
-     
-     
-        final_data = data2.to_dict(orient = "records")
-        return render(request, 'budget.html', {'final_data': final_data})
-       
-        print(final_data)
-    except KeyError:
-        print("failed")
-
-def index(request):
-    name = " index"
-    return render(request, 'index.html', {'name':name})
-    
-=======
-        # we don't need percentage... dropping it
->>>>>>> 629df80cafc47c82598accb2d314409392601166
         data2.drop(["percentage"], axis=1, inplace=True)
         final_data = data2.to_dict(orient="records")
         return render(request, 'budget.html', {'final_data': final_data})
 
     except KeyError:
         print("failed")
->>>>>>> c35c04aa26cc8d6570dd2a9e8936dcd98f419868
+
+
+def index(request):
+    name = " index"
+    return render(request, 'index.html', {'name': name})
