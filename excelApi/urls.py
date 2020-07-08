295184afbb344a4204776aa0f39b491aa5b62b90
frank9th <ap.teamservices@gmail.com>
2020-07-08 23:17:28 +0100

    removed .html extention from enpoint

--- excelApi/urls.py
+++ excelApi/urls.py
@@ -3,12 +3,9 @@ from . import views
 
 
 urlpatterns = [
-<<<<<<< HEAD
+
 path('', views.index, name='index'),
-path('budget.html', views.budget, name='budget'),
+path('budget', views.budget, name='budget'),
   
 ]
-=======
-    path('', views.budget, name='budget')
-]
->>>>>>> c35c04aa26cc8d6570dd2a9e8936dcd98f419868
+
--- excelApi/views.py
+++ excelApi/views.py
@@ -4,24 +4,6 @@ import pandas as pd
 # Create your views here.
 
 
-def budget(request):
-    try:
-        # reading the excel file
-        df = pd.read_excel('./media/APRIL.xlsx', usecols="B:G", encoding='utf-8')
-
-        # Dropping the unnecessary columns
-        data = df.dropna(axis=0, how="any")
-        data.columns = data.iloc[0]
-        data2 = data.iloc[1:, ].reindex()
-        # data3 = df.book.nrows
-        nrows = 10
-     
-        # here is month, the variable in which the month is stored in
-        # month = data2.columns[2]
-        data2.columns = data2.columns.map(lambda x: x.replace('\n', ''))
-        data2.columns = ["sector", "budget", "allocation", "total_allocation", "balance", "percentage"]
-
-<<<<<<< HEAD
 
 def budget(request):
 
@@ -56,12 +38,3 @@ def index(request):
     name = " index"
     return render(request, 'index.html', {'name':name})
     
-=======
-        # we don't need percentage... dropping it
-        data2.drop(["percentage"], axis=1, inplace=True)
-        final_data = data2.to_dict(orient="records")
-        return render(request, 'budget.html', {'final_data': final_data})
-
-    except KeyError:
-        print("failed")
->>>>>>> c35c04aa26cc8d6570dd2a9e8936dcd98f419868
