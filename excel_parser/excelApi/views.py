d2a17d10b9c8f220a2e2b9841e18ce9deff0c765
frank9th <ap.teamservices@gmail.com>
2020-07-08 17:45:51 +0100

    added temples and create urls

--- excel_parser/excelApi/views.py
+++ excel_parser/excelApi/views.py
@@ -16,30 +16,34 @@ from django.views.decorators.csrf import csrf_exempt
 # print(df.head())
 
 
-try:
-   
-    # reading the excel file
-    df = pd.read_excel('./media/APRIL.xlsx', usecols = "B:G",encoding='utf-8' )
-    # Dropping the unneccessary columns
-    data = df.dropna(axis = 0, how= "any")
-    data.columns = data.iloc[0]
-    data2 = data.iloc[1:,].reindex()    
-    # data3 = df.book.nrows
-    nrows = 10
- 
-    # here is month, the variable in which the month is stored in
-    #month = data2.columns[2]
- 
-    data2.columns = data2.columns.map(lambda x: x.replace('\n', ''))
-    data2.columns = ["sector", "budget", "allocation","total_allocation","balance","percentage"]
-    # we dont need percentage... dropping it
-    data2.drop(["percentage"], axis = 1, inplace = True)
- 
- 
-    final_data = data2.to_dict(orient = "records")
-   
-    print(final_data)
-except KeyError:
-    print("failed")
+
+def budget(request):
+
+    try:
+       
+        # reading the excel file
+        df = pd.read_excel('./media/APRIL.xlsx', usecols = "B:G",encoding='utf-8' )
+        # Dropping the unneccessary columns
+        data = df.dropna(axis = 0, how= "any")
+        data.columns = data.iloc[0]
+        data2 = data.iloc[1:,].reindex()    
+        # data3 = df.book.nrows
+        nrows = 10
+     
+        # here is month, the variable in which the month is stored in
+        #month = data2.columns[2]
+     
+        data2.columns = data2.columns.map(lambda x: x.replace('\n', ''))
+        data2.columns = ["sector", "budget", "allocation","total_allocation","balance","percentage"]
+        # we dont need percentage... dropping it
+        data2.drop(["percentage"], axis = 1, inplace = True)
+     
+     
+        final_data = data2.to_dict(orient = "records")
+        return render(request, 'budget.html', {'final_data': final_data})
+       
+        print(final_data)
+    except KeyError:
+        print("failed")
 
     
