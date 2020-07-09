ff6ca7088d2e13d9e0616c85d18a589a55bfb537
Oluwadamilola Sonaike <56271955+damiso15@users.noreply.github.com>
2020-07-09 15:03:38 +0100

    The backend for file upload (#160)
    * commit
    
    * commit
    
    * commit
    
    * nuke
    
    * commit
    
    * Templates and static directories  (#146)
    
    * Added templates, update views and URLS
    
    * Added templates, update views and URLS
    
    * Created landing page for api
    
    * Created landing page for api
    
    * comit
    
    * comit
    
    * comit
    
    * yo
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    Co-authored-by: Frank Moses <55638637+frank9th@users.noreply.github.com>

--- parse/views.py
+++ parse/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ExcelUploadForm
 
 # Create your views here.
+
+
+def model_form_upload(request):
+    if request.method == 'POST':
+        form = ExcelUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ExcelUploadForm()
+    return render(request, 'parse/model_form_upload.html', {'form': form})
