7d244e25c0b286077608cd32a91e7ccfa8010ca4
damiso15 <damiso15@yahoo.com>
2020-07-09 08:51:37 +0100

    commit

--- excel_parser/settings.py
+++ excel_parser/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['**.heroku.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
