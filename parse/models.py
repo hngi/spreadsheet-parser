6ce0612053252bcb6c7c2b515c1ff0c10103bae3
damiso15 <damiso15@yahoo.com>
2020-07-09 14:55:52 +0100

    commit

--- parse/models.py
+++ parse/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ExcelUpload(models.Model):
+    name = models.CharField(max_length=25)
+    document = models.FileField(upload_to='user/')
+    upload_at = models.DateTimeField(auto_now_add=True)
