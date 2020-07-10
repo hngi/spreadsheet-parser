aba98b1be93708683f96ef826472a274923b1322
Abdulmalik Giwa <giwa.omotola@gmail.com>
2020-07-10 04:30:50 +0100

    Swagger UI (#161)

--- excel_parser/settings.py
+++ excel_parser/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY =  os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'excelApi',
+    'drf_yasg',
     'parse',
 ]
 
