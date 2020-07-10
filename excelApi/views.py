505cb0307059f55b34b9545d2a1e3ec6ef74c782
damiso15 <damiso15@yahoo.com>
2020-07-10 13:27:14 +0100

    feature delete script

--- excelApi/views.py
+++ excelApi/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 import pandas as pd
 from .forms import LinkUploadForm
---
+++ parse/delete_script.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import time
+import os
+from excel_parser.settings import BASE_DIR
+
+
+def clear_directory():
+    while True:
+        time_list = []
+        now = time.mktime(datetime.now().timetuple())
+        directory = os.path.join(BASE_DIR, 'media/user')
+        for file in os.listdir(directory):
+            file = os.path.join(directory, file)
+            # get file creation/modification time
+            file_time = os.path.getmtime(file)
+            if now - file_time > 10:
+                os.remove(file)
+            else:
+                # add time info to list
+                time_list.append(file_time)
+
+        # after check all files, choose the oldest file creation time from list
+        # if time_list is empty, set sleep time as 300 seconds, else calculate it based on the oldest file creation time
+        sleep_time = (now - min(time_list)) if time_list else 15
+        time.sleep(sleep_time + 5)
+
+
+print(clear_directory())
