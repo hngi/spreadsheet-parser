11d8f148d09adb02d54be5e1c2b7d2db41d052f0
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-07-17 03:36:03 +0100

    adding upload link views

--- parse/cdn_forms.py
+++ parse/cdn_forms.py
@@ -11,7 +11,7 @@ from .models import CDNUpload
 class CDNUploadForm(forms.Form):
     class Meta:
         model = CDNUpload
-        widget = forms.TextInput(
+        link = forms.TextInput(
             attrs={
                     "position": "absolute;",
                     "width": "755px;",
