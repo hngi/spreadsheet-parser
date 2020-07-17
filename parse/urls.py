b908ab0fafbeda1b8b385749ab53260a72f33eb9
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-07-17 13:24:59 +0100

    hello

--- parse/urls.py
+++ parse/urls.py
@@ -6,19 +6,13 @@ app_name = 'parse'
 
 urlpatterns = [
     path('', views.index, name='home'),
-    #path('index/', cdn_views.cdn_parse, name='index'),
+     path('about/', views.about, name='about'),
     path('excel_to_csv/', views.excel_parse_to_csv, name = "excel"),
-    path('exceltopdf/', views.excel_to_pdf, name = "pdf"),
-
     path('upload/', views.form_upload, name='models_form_upload'),
     path('linkupload/', cdn_views.cdn_upload, name= "linkupload"),
-   # path('resultshow/', cdn_views.excel_parse, name='result')
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('json_parser/', views.excel_parse_to_json, name='json-parser'),
-    path('', views.index, name='home'),
    # path('index/', cdn_views.cdn_json_parse, name='index'),
-    path('upload/', views.form_upload, name='models_form_upload'),
-    path('linkupload/', cdn_views.cdn_upload, name= "linkupload"),
-    # path('resultshow/', cdn_views.excel_parse, name='result')
-
+# path('resultshow/', cdn_views.excel_parse, name='result')
+  
 ]
