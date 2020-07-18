67a12b2c76e7076f7ffa0ab31ea81800dc38090a
Alabi Feranmi <57694834+ferrumie@users.noreply.github.com>
2020-07-18 08:18:40 +0100

    user downloads now deletes automatically (#198)
    * adding excel to pdf view
    
    * requirements
    
    * adjusting templates
    
    * okay
    
    * conflict
    
    * spliting the buttons to return each files
    
    * adding download functionality
    
    * hello
    
    * adding upload link views
    
    * hello
    
    * sliced about us page
    
    * fixed shols name
    
    * fixed shols name
    
    * saving as filename
    
    * del
    
    * m
    
    * :m
    
    * l
    
    * final touches
    
    * delete files after parsing
    
    * deleting unnecessary files
    
    * remove

--- parse/views.py
+++ parse/views.py
@@ -10,13 +10,18 @@ from django.core.files.storage import FileSystemStorage
 import json
 from django.http import HttpResponse
 from pathlib import Path
+from .delete_script import clear_download
+
+
 # Create your views here.
 #landing page view
 def about(request):
     return render(request, "about_us.html")
+
 def index(request):
   #  excel_upload = ExcelUpload.objects.all()
     return render(request, 'landing_page.html')
+
 #view for form upload, it collects the file from the form and save temporarily to media/upload
 def form_upload(request):
     if request.method == 'POST':
@@ -24,80 +29,91 @@ def form_upload(request):
         fs = FileSystemStorage(location = 'media/upload')
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
+
         if 'csv' in request.POST:
             return redirect("parse:excel")  
         elif 'json' in request.POST:
             return redirect("parse:json-parser")
-        # return render(request, 'file_upload.html',{'uploaded_file_url':uploaded_file_url})
+
     elif request.method == "GET":
         return render(request, "file_upload.html")
+
 # view for parsing the excel file into json and returning the file for download
 def excel_parse_to_json(request):
-    # if request.POST.get('json'):
     directory = os.path.join(BASE_DIR, r'media\upload')
+    filename = ""
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
-    try:
-        if filename.endswith('.xlsx'):
-            file_name = os.path.join(directory, filename)
-            df = pd.read_excel(file_name, encoding='utf-8')
-            p= Path(file_name)
-            realname = p.stem
-            os.remove(file_name)
-            data = df.dropna(axis=0, how='any')
-            data.columns = data.columns.map(lambda x: str(x))
-            data.columns = data.columns.map(lambda x: x.replace('\n', ''))
-            final_data = data.to_dict(orient='records')
-            path2 = f"media/user/{realname}.json"
-            path3 =f"user/{realname}.json" 
-            with open(path2, 'w') as fp:
-                json.dump(final_data, fp)
-            #return render(request, 'download.html', {'final_data': final_data})
-            filep = os.path.join(settings.MEDIA_ROOT, path3)
-            if os.path.exists(filep):
-                with open(filep,'rb') as fh:
-                    response = HttpResponse(fh.read(), content_type='application/force-download')
+       # print(file)
+        try:
+            if filename.endswith('.xlsx'):
+                file_name = os.path.join(directory, filename)
+                df = pd.read_excel(file_name, encoding='utf-8')
+                p= Path(file_name)
+                realname = p.stem
+                os.remove(file_name)
+                data = df.dropna(axis=0, how='any')
+                data.columns = data.columns.map(lambda x: str(x))
+                data.columns = data.columns.map(lambda x: x.replace('\n', ''))
+                final_data = data.to_dict(orient='records')
+                path2 = f"media/user/{realname}.json"
+                path3 =f"user/{realname}.json" 
+                with open(path2, 'w') as fp:
+                    json.dump(final_data, fp)
+                filep = os.path.join(settings.MEDIA_ROOT, path3)
+                if os.path.exists(filep):
+                    
+                    with open(filep,'rb') as fh:
+                        response = HttpResponse(fh.read(), content_type='application/force-download')
                     response['Content-Disposition'] = 'inline; filename=' + os.path.basename(filep)
+                    os.remove(filep)
                     return response
-        else:
-            error = 'Ooops!! An error occurred. Please input an excel file(.xlsx).'
-            return render(request, 'file_upload.html', {'error':error})      
-    except KeyError:
-            error = 'Ooops!! Something went wrong in reading the contents of this excel file...'
-            return render(request, 'file_upload.html', {'error':error})  
-# view for parsing the excel file into csv and returning the file for download
+                                
+            else:
+                os.remove(f"media/upload/{filename}")
+                error = 'Ooops!! An error occurred. Please input an excel file(.xlsx).  '
+                return render(request, 'file_upload.html',{'error':error})     
+        except KeyError:
+                error = 'Ooops!! Something went wrong in reading the contents of this excel file...'
+                return render(request, 'file_upload.html', {'error':error})  
+    return redirect("parse:models_form_upload")
+
+
+        # view for parsing the excel file into csv and returning the file for download
 def excel_parse_to_csv(request):
-    # if request.POST.get('csv'):
     directory = os.path.join(BASE_DIR, r'media\upload')
+    filename = ""
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
-    try:
-        if filename.endswith('.xlsx'):
-            file_name = os.path.join(directory, filename)
-            file_path = f'media/upload/{filename}'
-            df = pd.read_excel(file_path, encoding='utf-8')
-            p= Path(file_path)
-            realname = p.stem
-            os.remove(file_path)
-            data = df.dropna(axis=0, how="any")
-            data.columns = data.columns.map(lambda x: str(x))
-            path = f"media/user/{realname}.csv"
-            data.to_csv(path, index=False)
-            path3 =f"user/{realname}.csv"
-            filep = os.path.join(settings.MEDIA_ROOT, path3)
-            if os.path.exists(filep):
-                with open(filep,'rb') as fh:
-                    response = HttpResponse(fh.read(), content_type='application/force-download')
+        try:
+            if filename.endswith('.xlsx'):
+                file_name = os.path.join(directory, filename)
+                file_path = f'media/upload/{filename}'
+                df = pd.read_excel(file_path, encoding='utf-8')
+                p= Path(file_path)
+                realname = p.stem
+                os.remove(file_path)
+                data = df.dropna(axis=0, how="any")
+                data.columns = data.columns.map(lambda x: str(x))
+                path = f"media/user/{realname}.csv"
+                data.to_csv(path, index=False)
+                path3 =f"user/{realname}.csv"
+                filep = os.path.join(settings.MEDIA_ROOT, path3)
+                if os.path.exists(filep):
+                    with open(filep,'rb') as fh:
+                        response = HttpResponse(fh.read(), content_type='application/force-download')
                     response['Content-Disposition'] = 'inline; filename=' + os.path.basename(filep)
+                    os.remove(filep)
                     return response
-        else:
-            error = 'Ooops!! An error occurred. Please input an excel file(.xlsx).'
-            return render(request, 'file_upload.html', {'error':error})      
-    except KeyError:
-            error = 'Ooops!! Something went wrong in reading the contents of this excel file...'
-            return render(request, 'file_upload.html', {'error':error})  
-    # except KeyError:
-    #     messages.error(request, "Operation Failed")
+            else:
+                os.remove(f"media/upload/{filename}")
+                error = 'Ooops!! An error occurred. Please input an excel file(.xlsx).  '
+                return render(request, 'file_upload.html',{'error':error})      
+        except KeyError:
+                error = 'Ooops!! Something went wrong in reading the contents of this excel file...'
+                return render(request, 'file_upload.html', {'error':error})  
+    return redirect("parse:models_form_upload")
+    
 
 
 
