55291bf7018dbf2c9ec64e529256b02f31c3f0ac
Alabi OluwaFeranmi <alabiemmanuelferanmi@gmail.com>
2020-07-18 11:44:15 +0100

    conflicting

--- parse/views.py
+++ parse/views.py
@@ -40,7 +40,7 @@ def form_upload(request):
 
 # view for parsing the excel file into json and returning the file for download
 def excel_parse_to_json(request):
-    directory = os.path.join(BASE_DIR, r'media\upload')
+    directory = os.path.join(BASE_DIR, f'media/upload')
     filename = ""
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
